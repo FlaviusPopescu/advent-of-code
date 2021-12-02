a2844dd25ba750f6480676c39f4f1425a543e9a3
flavius <flavius.dev>
2021-12-01 22:31:03 -0500

    Refactor day 1 solutions
    * part 1 doesn't need to buffer the input
    * both parts use the input extension, and do the final count
    using the same logic

--- aoc-2021/src/main/kotlin/Day01.kt
+++ aoc-2021/src/main/kotlin/Day01.kt
@@ -2,24 +2,23 @@
  * https://adventofcode.com/2021/day/1
  */
 object Day01 {
-    fun solvePart1(): Int {
-        var depthIncreases = 0
-        this::class.run {
-            java.getResourceAsStream("$simpleName.in").reader().forEachLine { newLine ->
-                depthIncreases += if (checkDepthIncreased(newLine) == true) 1 else 0
-            }
-        }
-        return depthIncreases
-    }
+    fun solvePart1(): Int = inputFromFile(::parseLine).totalIncreases()
 
-    private var previousDepth = Int.MAX_VALUE
-    private fun checkDepthIncreased(newReading: String) = newReading.trim().toIntOrNull()
-        ?.run {
-            (this > previousDepth)
-                .also { previousDepth = this }
+    fun solvePart2(): Int = inputFromFile(::parseLine)
+        .windowed(size = 3)
+        .map { window -> window.sum() }
+        .totalIncreases()
+
+    private fun List<Int>.totalIncreases(): Int = windowed(size = 2)
+        .sumOf { (previousDepth, currentDepth) ->
+            (if (currentDepth > previousDepth) 1 else 0).toInt()
         }
+
+    private fun parseLine(line: String): Int = line.trim().toIntOrNull()
+        ?: throw IllegalArgumentException("Unexpected input=[$line]")
 }
 
 fun main() {
-    println("Number of depth increases: ${Day01.solvePart1()}")
+    println("Part 1: Number of depth increases: ${Day01.solvePart1()}")
+    println("Part 2: Number of windowed increases: ${Day01.solvePart2()}")
 }
